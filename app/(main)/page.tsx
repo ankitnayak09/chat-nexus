d87473d130b88e1ef593eb8c3028efe2a7c7518b
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-18 15:09:23 +0530

    Add theme toggle component and dependencies

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/toggle-theme";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
 		<p className="text-3xl font-bold text-indigo-500">
 			<UserButton afterSignOutUrl="/" />
+			<ModeToggle />
 		</p>
 	);
 }
