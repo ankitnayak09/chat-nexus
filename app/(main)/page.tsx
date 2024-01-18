6f1dad097eba43b855ef8c26b7f73c62d098974d
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2024-01-18 14:50:59 +0530

    Pass UserButton Props inMain Page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
 	return (
 		<p className="text-3xl font-bold text-indigo-500">
-			<UserButton />
+			<UserButton afterSignOutUrl="/" />
 		</p>
 	);
 }
